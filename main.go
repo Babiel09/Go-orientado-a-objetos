6cef98b9e61d406321d2e62f15c6b4e6d21bae92
Gabriel Castro <estudosbabiel@gmail.com>
2024-10-10 20:31:08 -0300

    Aprendendo sobre structs

--- main.go
+++ main.go
@@ -2,21 +2,27 @@ package main
 
 import "fmt"
 
+//Para construir uma Struct eu preciso definir vários tipos dentro do tipo determiando
+type ContaUsuario struct {
+	titular string
+	agencia int
+	conta   int
+	saldo   float64
+}
+
 func main() {
 	//Criando o primeiro titular:
-	var titular1 string = "Gabriel"
-	var agencia1 int = 123
-	var conta int = 1
-	var saldo float64 = 234.56
-	fmt.Println("O titular ", titular1, " possui R$", saldo, " em sua conta bancária.")
-	fmt.Println("O titular ", titular1, " está cadastrado na agencia: ", agencia1, " e sau conta é: ", conta)
-
-	//Criando o segundo titular:
-	var titular2 string = "Ana Luiza de Lima"
-	var agencia2 int = 321
-	var conta2 int = 2
-	var saldo2 float64 = 23.56
-
-	fmt.Println("A titular ", titular2, " possui R$", saldo2, " em sua conta bancária.")
-	fmt.Println("A titular ", titular2, " está cadastrado na agencia: ", agencia2, " e sau conta é: ", conta2)
+	gabriel := ContaUsuario{
+		"Gabriel Castro",
+		123,
+		1,
+		8793.98,
+	}
+	ana := ContaUsuario{
+		"Ana Luiza (Irmã do Arthur)",
+		321,
+		2,
+		6272.87,
+	}
+	fmt.Println(gabriel, ana)
 }
